43fc9a9aac943537aa4d68c1ece4aa541d8651a2
Tristan Slater <hello@trslater.ca>
2020-10-19 14:51:33 -0700

    Add library for shared code

---
+++ odd_pascal/lib.py
@@ -0,0 +1,82 @@
+"""MPMP 16 Lib
+"""
+
+
+import math
+import itertools
+
+
+def percent_odd(m):
+    return 2*3**(math.log(m, 2))/m/(m + 1)
+
+
+def print_sierpinski(pt):
+    for row in pt:
+        for num in row:
+            if is_odd(num):
+                print("* ", end="")
+
+            else:
+                print("  ", end="")
+
+        print()
+
+
+def pt(d):
+    return ((math.comb(i, j) for j in range(i+1)) for i in range(d))
+
+
+def paired(iterable):
+    """Returns an iterator of every consecutive pair from an iterable"""
+
+    # Get two copies of permutation as iterators
+    p1, p2 = itertools.tee(iterable)
+    # Advance second permutation iterator by one
+    next(p2)
+
+    return zip(p1, p2)
+
+
+def is_odd(num):
+    return num % 2 == 1
+
+
+# print(f"{100*percent_odd(128)}%")
+
+    # d = 16
+
+    # print(3**math.log(d, 2))
+    # print(3**math.log(1, 2))
+    # print(3**math.log(2, 2))
+    # print(3**math.log(3, 2))
+    # print(3**math.log(4, 2))
+    # print(3**math.log(5, 2))
+    # print(3**math.log(6, 2))
+    # print(3**math.log(7, 2))
+    # print(3**math.log(8, 2))
+    # print(f"{100*2*3**math.log(d, 2)/d/(d + 1)}%")
+
+    # print(count_occurances(3003))
+    # for row in pt_skew(20, 8):
+    #     for num in row:
+    #         if is_odd(num):
+    #             print(f" {num:>6}*", end="")
+
+    #         else:
+    #             print(f" {num:>6} ", end="")
+
+
+    #     print()
+
+    # for row in pt(d):
+    #     for num in row:
+    #         if is_odd(num):
+    #             print("* ", end="")
+
+    #         else:
+    #             print("  ", end="")
+
+    #     for num in row:
+    #         print(f" {num:>5} ", end="")
+
+    #     print()
